82ad616e1a94d40fad1dd38afe90ee6eec0ec0ef
i-whammy <yas.iwami@gmail.com>
2020-05-07 23:11:44 +0900

    implemented random strategy

---
+++ src/main/kotlin/design/pattern/kt/strategy/RandomStrategy.kt
@@ -0,0 +1,15 @@
+package design.pattern.kt.strategy
+
+import kotlin.random.Random
+
+class RandomStrategy(private val seed: Int): Strategy {
+    private val random = Random(seed)
+
+    override fun nextHand(): Hand {
+        return Hand.getHand(random.nextInt(3))
+    }
+
+    override fun study(win: Boolean) {
+        // do nothing
+    }
+}
